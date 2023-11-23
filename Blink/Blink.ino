aa735f04ee8522ca085776bc61669dbe79585a72
Tyrn <a@b.com>
2023-11-23 19:42:09 +0300

    LED redefinition is unnecessary after all :)

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,9 +22,6 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
-#undef LED_BUILTIN
-#define LED_BUILTIN PC13
-
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
